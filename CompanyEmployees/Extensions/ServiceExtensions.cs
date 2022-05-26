0da4a10653588d9e68457d54989568e96aee286c
yousafkh <yousakfh18@gmail.com>
2022-05-26 15:19:46 +0500

    Add Cors Policy service

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors (this IServiceCollection services) => 
+            services.AddCors (options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+    }
+}
